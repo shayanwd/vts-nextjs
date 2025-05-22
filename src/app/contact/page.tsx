f6f9cc6c7234ca654075b2f67853c8858ea3933a
shayanwd <shayankhanwd@gmail.com>
2025-05-22 12:05:51 +0500

    xxasx

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import ContactFormPage from "@/components/contactComponents/ContactFormPage";
+
+
+export default function Contact() {
+    return (
+        <>
+            <ContactFormPage />
+        </>
+    );
+}
